2efd475c426218bfab43e8b911c247ffc67a4b3d
margherita-coderblock <margherita@coderblock.com>
2023-09-27 17:16:23 +0200

    feat: add clerk auth, modal component and zustand with hook

---
+++ app/(root)/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+
+import { Button } from "@/components/ui/button";
+import { Modal } from "@/components/ui/modal";
+import { UserButton } from "@clerk/nextjs";
+
+const SetupPage = () => {
+  return (
+    <div className="p-4">
+      <Modal
+        title="Test"
+        description="TestDescription"
+        isOpen={true}
+        onClose={() => {}}
+      >
+        Children
+      </Modal>
+    </div>
+  );
+};
+
+export default SetupPage;
