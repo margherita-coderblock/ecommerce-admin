7f9edd2707d74ef1134fe7b31aa7bf1143d65910
margherita-coderblock <margherita@coderblock.com>
2023-10-05 15:44:47 +0200

    wip: billboards

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["res.cloudinary.com", "images.unsplash.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
